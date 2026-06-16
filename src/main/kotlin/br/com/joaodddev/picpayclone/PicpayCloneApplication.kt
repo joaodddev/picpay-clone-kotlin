fde5610d1da47d0200a8cf6e187781e7a0bc8040
João Victor <joaodddev@gmail.com>
2026-06-16 07:24:07 -0300

    chore: initial project setup

---
+++ src/main/kotlin/br/com/joaodddev/picpayclone/PicpayCloneApplication.kt
@@ -0,0 +1,11 @@
+package br.com.joaodddev.picpayclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PicpayCloneApplication
+
+fun main(args: Array<String>) {
+	runApplication<PicpayCloneApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/joaodddev/picpayclone/PicpayCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.joaodddev.picpayclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PicpayCloneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
