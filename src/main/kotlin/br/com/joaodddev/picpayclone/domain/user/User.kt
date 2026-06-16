92a0a619d4431e0b73dbeb091acd401f1a458993
João Victor <joaodddev@gmail.com>
2026-06-16 08:01:36 -0300

    feat: implement UserDetails in User entity

--- src/main/kotlin/br/com/joaodddev/picpayclone/domain/user/User.kt
+++ src/main/kotlin/br/com/joaodddev/picpayclone/domain/user/User.kt
@@ -1,6 +1,9 @@
 package br.com.joaodddev.picpayclone.domain.user
 
 import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 import java.math.BigDecimal
 
 @Entity
@@ -29,7 +32,24 @@ data class User(
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     val userType: UserType = UserType.COMMON
-)
+
+) : UserDetails {
+
+    override fun getAuthorities(): Collection<GrantedAuthority> =
+        listOf(SimpleGrantedAuthority("ROLE_${userType.name}"))
+
+    override fun getPassword(): String = password
+
+    override fun getUsername(): String = email
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
 
 enum class UserType {
     COMMON,
